8f142a8285e6892a8244e765c96cb46198f53ed9
gracewang <gwang2@mit.edu>
2025-11-12 22:42:51 -0500

    implemented perplexity

--- eval.py
+++ eval.py
@@ -24,6 +24,38 @@ def load_model(path, device):
 def perplexity(model, data_loader, device):
     # TODO: Implement perplexity calculation
     # YOUR CODE HERE
-    raise NotImplementedError()
+    model.eval()
+
+    print("Calculating perplexity of model with test prompts (batched):")
+    print("=" * 60)
+
+    total_loss = 0.0
+    total_tokens = 0
+
+    with torch.no_grad():
+        for x, y in tqdm(data_loader, desc="Calculating Perplexity"):
+            x, y = x.to(device), y.to(device)
+
+            # Forward pass
+            logits = model(x)
+            B, T, V = logits.shape
+
+            # Compute cross-entropy loss
+            loss = F.cross_entropy(logits.view(B * T, V), y.view(B * T), reduction='none')
+
+            # Update total_loss & total_tokens
+            total_loss += loss.sum().item()
+            total_tokens += y.numel()   # number of elements
+
+        avg_loss = total_loss / max(1, total_tokens)
+        avg_perplexity = float(torch.exp(torch.tensor(avg_loss)))
+
+        print(f"Average Perplexity: {avg_perplexity:.4f}")
+        return avg_perplexity
 
 # TODO other metrics we said we would implement
+def selective_copy_task(model, device):
+  raise NotImplementedError()
+
+def induction_task(model, device):
+  raise NotImplementedError()
